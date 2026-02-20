0d9bfa962f73b5b4d79e1b8f09ed39a010531fa0
AbirHossenCSE <abir.cse4@gmail.com>
2026-02-20 15:13:35 +0600

    post method

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
+import { postService } from "./post.service";
 
 const createPost = (req: Request, res: Response) => {
-    res.send("Create a new post");
-    console.log({ req, res });
+    try {
+        const result = postService.createPost(req.body);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to create post" });
+    }
 
 }
 
