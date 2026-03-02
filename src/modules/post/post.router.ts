2a3b65bc75b5e99a687c2feba1e1c73a72271607
AbirHossenCSE <abir.cse4@gmail.com>
2026-03-02 14:59:18 +0600

    new

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,3 +1,16 @@
+// import express, { Router } from "express";
+// import { postController } from "./post.controller";
+
+// const router = express.Router();
+
+// router.post(
+//     "/",
+//     postController.createPost
+// )
+
+// export const postRouter: Router = router;
+
+
 import express, { Router } from "express";
 import { postController } from "./post.controller";
 
