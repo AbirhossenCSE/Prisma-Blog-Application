6aab9cdc8fdbc93cbfaf4b392ecbdc30969d9a59
AbirHossenCSE <abir.cse4@gmail.com>
2026-03-03 14:27:04 +0600

    prisma

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,16 +1,3 @@
-// import express, { Router } from "express";
-// import { postController } from "./post.controller";
-
-// const router = express.Router();
-
-// router.post(
-//     "/",
-//     postController.createPost
-// )
-
-// export const postRouter: Router = router;
-
-
 import express, { Router } from "express";
 import { postController } from "./post.controller";
 
